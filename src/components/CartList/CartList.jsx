68479693021cd5516d747a359bf090c40cb3a189
Ivan Malenko <ivan.malenko18@gmail.com>
2023-05-28 10:38:02 +0300

    added goods in cart

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,12 +1,19 @@
 import { fetchById } from 'API/api';
 import { MyContext } from 'components/App';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useContext } from 'react';
 
 export const CartList = () => {
-  const [shop, setShop] = useEffect({});
+  const [shop, setShop] = useState({});
+  const [productQuantities, setProductQuantities] = useState({});
+  const { cart } = useContext(MyContext);
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (cart.length !== 0) {
+      getShopById(cart[0].shopId);
+    }
+  }, [cart, cart.length]);
 
   const getShopById = async id => {
     try {
@@ -16,8 +23,28 @@ export const CartList = () => {
       console.log(error);
     }
   };
+  let productsInCartId = [];
 
-  const { cart } = useContext(MyContext);
+  let productsInCart;
   console.log(cart);
-  return <></>;
+
+  //   console.log(productsInCartId);
+  if (cart.length !== 0 && shop.products !== undefined) {
+    cart.forEach(el => productsInCartId.push(el.productId));
+    // console.log(shop.products);
+    productsInCart = shop.products.filter(element =>
+      productsInCartId.includes(element.id)
+    );
+  }
+
+  //   console.log(productsInCart);
+
+  return (
+    <>
+      <ul>
+        {productsInCart &&
+          productsInCart.map(el => <li key={el._id}>{el.name}</li>)}
+      </ul>
+    </>
+  );
 };
